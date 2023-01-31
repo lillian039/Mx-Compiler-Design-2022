204640a7eacf022ae2348411565c4c0a7d4dd7bf
lillian039 <1912383615@qq.com>
2023-01-31 14:43:22 +0800

    remove .ll

---
+++ src/MiddleEnd/IRMem2Reg.java
@@ -0,0 +1,109 @@
+package MiddleEnd;
+
+import LLVMIR.BasicBlock;
+import LLVMIR.Expression.*;
+import LLVMIR.GlobalDefine.ClassDef;
+import LLVMIR.GlobalDefine.FuncDef;
+import LLVMIR.GlobalDefine.VarDef;
+import LLVMIR.IRVisitor;
+import LLVMIR.RootIR;
+import LLVMIR.Terminal.Branch;
+import LLVMIR.Terminal.Jump;
+import LLVMIR.Terminal.Ret;
+
+public class IRMem2Reg implements IRVisitor {
+    @Override
+    public void visit(Alloca it) {
+
+    }
+
+    @Override
+    public void visit(Binary it) {
+
+    }
+
+    @Override
+    public void visit(Bitcast it) {
+
+    }
+
+    @Override
+    public void visit(Call it) {
+
+    }
+
+    @Override
+    public void visit(GetElementPtr it) {
+
+    }
+
+    @Override
+    public void visit(Icmp it) {
+
+    }
+
+    @Override
+    public void visit(Load it) {
+
+    }
+
+    @Override
+    public void visit(Malloc it) {
+
+    }
+
+    @Override
+    public void visit(Store it) {
+
+    }
+
+    @Override
+    public void visit(Trunc it) {
+
+    }
+
+    @Override
+    public void visit(Zext it) {
+
+    }
+
+    @Override
+    public void visit(Branch it) {
+
+    }
+
+    @Override
+    public void visit(Jump it) {
+
+    }
+
+    @Override
+    public void visit(Ret it) {
+
+    }
+
+    @Override
+    public void visit(FuncDef it) {
+
+    }
+
+    @Override
+    public void visit(ClassDef it) {
+
+    }
+
+    @Override
+    public void visit(VarDef it) {
+
+    }
+
+    @Override
+    public void visit(BasicBlock it) {
+
+    }
+
+    @Override
+    public void visit(RootIR it) {
+
+    }
+}
